d93897365bc410f331893e15df4fae98776525be
angel-gamis <113718007+angel-gamis@users.noreply.github.com>
2022-11-08 13:59:11 -0600

    Movement Finished
    Needs Animation + Pipes

--- JSLABS5/JSLABS5_2/main.js
+++ JSLABS5/JSLABS5_2/main.js
@@ -3,9 +3,12 @@ const player = document.getElementById('player');
 // ** Player Variables **
 let playerY = 250;
 let playerMoveStatus = 0;
-let playerBoost = 50;
-let playerFall = 20;
+let playerBoost = 100;
+let playerFall = 2;
 
+// ** Game Settings **
+let start = true;
+setInterval(updatePosition, 15);
 
 
 document.onkeydown = checkKeycode;
@@ -15,7 +18,7 @@ let Keys = {
     up: false,
     space: false
 }
-
+ 
 function checkKeycode(e){
     let keycode = e.keyCode;
     e.preventDefault();
@@ -50,11 +53,28 @@ function move(){
     if(start == true){
         // ** Player Jump **
         if(Keys.up == true){
-            player.; // Up
+            playerY -= playerBoost; // Up
+            player.style.backgroundimage = 'url(birdup1.gif)';
+            setTimeout(function(){
+                player.style.backgroundimage = 'url(birdrest.gif)'
+            },1000) 
         }
         if(Keys.space == true){
+            playerY -= playerBoost; // Up
+            playerMoveStatus = 1;
+            player.style.backgroundimage = 'url(birdup1.gif)';
+            setTimeout(function(){
+                player.style.backgroundimage = 'url(birdrest.gif)'
+                console.log("back to rest")
+            },2000) 
 
         }
     }
+    player.style.top = playerY + "px";
+    console.log('position: ' + playerY)
+}
 
+function updatePosition(){
+    playerY += playerFall;
+    player.style.top = playerY + "px";
 }
