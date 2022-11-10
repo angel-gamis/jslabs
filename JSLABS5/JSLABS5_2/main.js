ea16aba09e5ea13e99dbff5c6488f40f9dfd0ec0
angel-gamis <113718007+angel-gamis@users.noreply.github.com>
2022-11-09 21:44:26 -0600

    Floor Animations Working

--- JSLABS5/JSLABS5_2/main.js
+++ JSLABS5/JSLABS5_2/main.js
@@ -1,4 +1,7 @@
 const player = document.getElementById('player');
+const grass = document.getElementById('grass');
+const newGrass = document.getElementById('newGrass');
+const screen = document.getElementById('screen');
 
 // ** Player Variables **
 let playerAlive = true;
@@ -9,13 +12,17 @@ let playerFall = 2;
 
 // ** Game Settings **
 let start = true;
-setInterval(playerFalling, 450)
+setInterval(playerFalling, 450);
 setInterval(updatePosition, 15);
 setInterval(checkGameStatus, 10);
 let runKeyPress = true;
 
 // ** World Settings **
 let floor = 525;
+let grassX = 2;
+let newGrassX = 478;
+let grassSpeed = 3;
+let grassDeleteX = -480;
 
 document.onkeypress = checkKeycode;
 document.onkeyup = checkKeycodeUp;
@@ -24,35 +31,35 @@ let Keys = {
     up: false,
     space: false
 }
- 
-function checkKeycode(e){
+
+function checkKeycode(e) {
     let keycode = e.keyCode;
     e.preventDefault();
-        if (!runKeyPress) return;
-        if (!playerAlive) return;
+    if (!runKeyPress) return;
+    if (!playerAlive) return;
 
-        if(keycode == 38 || keycode == 87 ){
-            runKeyPress = false;
-            Keys.up = true; // Up
-        }
-        if(keycode == 32){
-            runKeyPress = false;
-            Keys.space = true; // Space
-        }
+    if (keycode == 38 || keycode == 87) {
+        runKeyPress = false;
+        Keys.up = true; // Up
+    }
+    if (keycode == 32) {
+        runKeyPress = false;
+        Keys.space = true; // Space
+    }
 
     move();
-    
+
 }
 
-function checkKeycodeUp(e){
+function checkKeycodeUp(e) {
     let keycode = e.keyCode;
     e.preventDefault();
 
-    if(keycode == 38 || keycode == 87){
+    if (keycode == 38 || keycode == 87) {
         Keys.up = false; // Up
-        runKeyPress = true;        
+        runKeyPress = true;
     }
-    if(keycode == 32){
+    if (keycode == 32) {
         Keys.space = false; // Space
         runKeyPress = true;
     }
@@ -60,26 +67,26 @@ function checkKeycodeUp(e){
     move();
 }
 
-function move(){ 
+function move() {
 
-    if(start == true){
+    if (start == true) {
         // ** Player Jump **
-        if(Keys.up == true){
+        if (Keys.up == true) {
             playerY -= playerBoost; // Up
             playerMoveStatus = 1;
             player.style.backgroundImage = 'url(birdup1.gif)';
-            setTimeout(function(){
+            setTimeout(function () {
                 player.style.backgroundImage = 'url(birdrest.gif)'
-            },600) 
+            }, 600)
         }
-        if(Keys.space == true){  
+        if (Keys.space == true) {
             playerY -= playerBoost; // Up
             playerMoveStatus = 1;
             player.style.backgroundImage = 'url(birdup1.gif)';
-            setTimeout(function(){
+            setTimeout(function () {
                 player.style.backgroundImage = 'url(birdrest.gif)'
                 console.log("back to rest")
-            },600) 
+            }, 600)
 
         }
     }
@@ -87,26 +94,38 @@ function move(){
     //console.log('position: ' + playerY)
 }
 
-function updatePosition(){
+function updatePosition() {
     if (!playerAlive) return;
     playerY += playerFall;
     player.style.top = playerY + "px";
-    //console.log('position: ' + playerY);
-    if(playerMoveStatus <= 0){
+    if (playerMoveStatus <= 0) {
         player.style.backgroundImage = 'url(birddown1.gif)';
     }
-    if(playerY >= floor){
+    if (playerY >= floor) {
         player.style.backgroundImage = 'url(birddead.gif)';
         playerAlive = false;
     }
+
+    // Move grass to beginning
+    if (newGrassX <= -478) {
+        newGrassX = 478;
+    }
+    if (grassX <= -478) {
+        grassX = 478;
+    }
+
+    newGrassX -= grassSpeed;
+    grassX -= grassSpeed;
+    grass.style = "position: absolute; left: " + grassX + "px; top: 558px;";
+    newGrass.style = "position: absolute; left: " + newGrassX + "px; top: 558px;";
 }
 
-function playerFalling(){
+function playerFalling() {
     playerMoveStatus -= .5;
 }
 
-function checkGameStatus(){
-    if(playerAlive = false){
+function checkGameStatus() {
+    if (playerAlive == false) {
         gameover = document.createElement('img');
     }
 }
