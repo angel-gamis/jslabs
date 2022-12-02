ac69b4eeb57f3792c4ada8ac662b6218274a59db
angel-gamis <113718007+angel-gamis@users.noreply.github.com>
2022-12-02 13:59:42 -0600

    Score Loads

--- JSLABS5/JSLABS5_2/main.js
+++ JSLABS5/JSLABS5_2/main.js
@@ -7,6 +7,9 @@ const pipeTop = document.getElementById('pipeTop');
 const pipeBottom = document.getElementById('pipeBottom');
 const pipeExtra = document.getElementById('pipeExtra');
 const hole = document.getElementById('hole');
+const score1 = document.getElementById('score1');
+const score2 = document.getElementById('score2');
+const score3 = document.getElementById('score3');
 
 // ** Player Variables **
 let playerAlive = true;
@@ -16,6 +19,8 @@ let playerMoveStatus = 1;
 let playerBoost = 100;
 let playerFall = 3.75;
 let flapCount = 0;
+let scoreCount = 0;
+let scoreNumbers = 1;
 
 // ** Game Settings **
 let start = false;
@@ -38,6 +43,17 @@ pipeTop.addEventListener('animationiteration', () => {
     if((randomBottom - 377) >= 0){
         pipeExtra.style = "position: absolute; top: 0px; left: 399px";
     }
+
+    scoreCount++;
+    score1.src = String(scoreCount)[0] + ".png"
+    if(scoreCount >= 10 && scoreCount < 100){
+        scoreNumbers++;
+        score1.style = "position: absolute; height: 50px; left: 200px; top: 50px;"
+        score2.style = "position: absolute; height: 50px; left: 230px; top: 50px;"
+    }
+    if(scoreCount >= 100){
+        scoreNumbers++;
+    }
     // bottom 515px max 217 min (363 &< needs Extra)
     // top -150px min max 148px (-1 &> needs Extra)
 })
@@ -54,6 +70,7 @@ document.onkeyup = checkKeycodeUp;
 
 document.body.onload = function(){
     player.classList = "start flap";
+    score1.style = "height: 50px; position: absolute; left: 215px; top: 50px;"
 }
 
 let Keys = {
@@ -175,6 +192,7 @@ function playerFalling() {
 }
 
 function animateFlap(){
+    if(!playerAlive) return;
     flapCount++
     console.log("flap")
     if(flapCount == 4){
@@ -195,17 +213,64 @@ function animateFlap(){
 }
 
 function checkGameStatus() {
-    console.log(playerAlive)
     if (playerAlive == false && gameOver == false) {
+
+        // - Gameover Board
         let gameover = document.createElement('img');
         gameover.src = "scoreBoard.png";
         gameover.classList = "scoreBoard";
+
+        // - Score Img
+        let scoreImg = document.createElement('img');
+        scoreImg.src = "score.png";
+        scoreImg.classList = "scoreImg";
+
+        // - Best Img
+        let bestImg = document.createElement('img');
+        bestImg.src = "best.png";
+        bestImg.classList = "bestImg";
+
+        // - Score UI
+        let scoreUI = document.createElement('img');
+        let scoreUI2 = document.createElement('img');
+        let scoreUI3 = document.createElement('img');
+        let scoreUI4 = document.createElement('img');
+        if(scoreNumbers == 1){
+            scoreUI.src = scoreCount + ".png";
+            scoreUI.style = "height: 35px; position: absolute; left: 235px; top: 290px;";
+        }
+        if(scoreNumbers == 2){
+            scoreUI.src = String(scoreCount)[0] + ".png";
+            scoreUI.style = "height: 35px; position: absolute; left: 220px; top: 290px;";
+            scoreUI2.src = String(scoreCount)[1] + ".png";
+            console.log(String(scoreCount)[1]);
+            console.log(scoreCount);
+            scoreUI2.style = "height: 35px; position: absolute; left: 250px; top: 290px;";
+        }
+
+        // - Restart Button
         let restartBtn = document.createElement('img');
-        restartBtn.src = " restartBtn.png";
-        restartBtn.classList = "restartBtn";
+        restartBtn.src = "restartBtn.png";
+        restartBtn.classList = "restartBtn grow";
+
+        // - Share Button
+        let shareBtn = document.createElement('img');
+        shareBtn.src = "shareBtn.png";
+        shareBtn.classList = "shareBtn grow";
+
+        // Game Over is True
         gameOver = true;
-        imgContainer.appendChild(restartBtn);
+
+        // Append All UI
         imgContainer.appendChild(gameover);
+        imgContainer.appendChild(scoreImg);
+        imgContainer.appendChild(bestImg);
+        if(scoreNumbers == 2){
+            imgContainer.appendChild(scoreUI2);
+        }
+        imgContainer.appendChild(scoreUI);
+        imgContainer.appendChild(shareBtn);
+        imgContainer.appendChild(restartBtn);
         pipeBottom.classList = "";
         pipeTop.classList = "";
         pipeExtra.classList = "";
